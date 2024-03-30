913917159b2c7ad19d6caf54778ca2ed78c77c4e
VuNDU-FE <vu.ndu@picontechnology.com>
2024-03-30 17:58:55 +0700

    [30/03/2024]

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -2,12 +2,13 @@ import jwt from 'jsonwebtoken'
 
 export default defineEventHandler((event) => {
   const accessToken = getHeader(event, 'Authorization')
-  const role = getCookie(event, 'role')
+  const role = getHeader(event, 'Role')
 
   const pathList = ['/api/products', '/api/categories', '/api/oders']
 
   try {
-    if (!pathList.find((item) => getRequestPath(event).includes(item))) return
+    if (!pathList.find((item) => getRequestPath(event).includes(item)) || !role)
+      return
 
     jwt.verify(accessToken || '', process.env.JWT_SECRET || '')
   } catch (error) {
